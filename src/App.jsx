95c2117587def19792d26dd4c1720b6e7ef2ba35
Aakash Podar <164460415+Ap230824@users.noreply.github.com>
2024-06-04 17:38:54 +0530

    main files

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar.jsx";
+import Hero from "./Components/Hero/Hero.jsx";
+import Programs from "./Components/Programs/Programs.jsx";
+import Title from "./Components/Title/Title.jsx";
+import About from "./Components/About/About.jsx";
+import Campus from "./Components/Campus/Campus.jsx";
+import Testimonials from "./Components/Testimonials/Testimonials.jsx";
+import Contact from "./Components/Contact/Contact.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
+
+
+const App = ()=>{
+  return(
+    <div>
+       <Navbar/>
+        <Hero/>
+       <div className="container">
+        <Title subTitle='Our PROGRAM' title='What We Offer'/>
+       <Programs/>
+      <About/>
+      <Title subTitle='Gallery' title='Campus Photos'/>
+      <Campus/>
+
+      <Title subTitle='TESTIMONIALS' title='What Student Says'/>
+      <Testimonials/>
+
+      <Title subTitle='Contact us' title='Get in Touch'/>
+      <Contact/>
+      <Footer/>
+       </div>
+    </div>
+  );
+}
+
+
+export default App;
