cefc0ff65022e3f94b43db5ec651a546387772a2
Aakash Podar <164460415+Ap230824@users.noreply.github.com>
2024-07-23 19:05:27 +0530

    Update Campus.jsx

--- src/Components/Campus/Campus.jsx
+++ src/Components/Campus/Campus.jsx
@@ -10,10 +10,12 @@ const Campus = () => {
   return (
     <div className='campus'>
       <div className="gallery">
+        
         <img src={gallery_1} alt="" />
         <img src={gallery_2} alt="" />
         <img src={gallery_3} alt="" />
         <img src={gallery_4} alt="" />
+        
       </div>
 
       <button className='btn dark-btn'> See More here <img src={white_arrow} alt="" /></button>
